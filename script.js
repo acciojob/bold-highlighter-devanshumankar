760ea6ee2091df270587c9985cd0a08d6d1699ba
Devanshu Chandu Mankar <158685271+devanshumankar@users.noreply.github.com>
2025-11-19 15:38:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+	const link=document.querySelector("a");
+	link.addEventListener("mouseover",()=>{
+		const strongs=document.querySelectorAll("strong");
+		strongs.forEach((strong)=>{
+			strong.style.color="green";
+		})
+	})
 
 }
-
-
 function return_normal() {
     //Write your code here
-
+	const strongs=document.querySelectorAll("strong");
+		strongs.forEach((strong)=>{
+			strong.style.color="black";
+		})
     
 }
